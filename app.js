208977bdf8d87be6ae160d23b67c3390a74eb69d
balachander <balachander7315@gmail.com>
2021-10-21 08:11:52 +0530

    async and await

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-const table=document.querySelector("table")
+// const table=document.querySelector("table")
 
-let response=fetch("https://jsonplaceholder.typicode.com/users")
-.then(res=> res.json())
-.then(data=>{
-  data.forEach(user => {
-    let  r =document.createElement("tr")
-    let did =document.createElement("td")
-    let dit =document.createTextNode(user.id)
-    did.appendChild(dit)
-    r.appendChild(did)
-    table.appendChild(r)
-    
-  });
+// let response=fetch("https://jsonplaceholder.typicode.com/users")
+// .then(res=> res.json())
+// .then(data=>{
+//   console.log(data)
 
-})
+//   })
+const fetchData = async ()=>{
+  const response= await fetch("https://jsonplaceholder.typicode.com/users")
+  console.log(response)
+  const data= await response.json()
+  console.log(data)
+
+ 
+}
+fetchData()
